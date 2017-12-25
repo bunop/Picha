1055b31167131d1715a15bbb51bc9d8d7b713af0
Abhyuday Pratap Singh <abhyudaypratap@outlook.com>
2017-12-25 12:54:03 +0530

    chore(project): updated for django=2.0

--- picha/urls.py
+++ picha/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 from photos.views import PhotoView
 from feedback.views import FeedbackView
 
 urlpatterns = [
-    url(r'^$', PhotoView.as_view(), name="home"),
-    url(r'^feedback/$', FeedbackView.as_view(), name="feedback"),
-    url(r'^admin/', include(admin.site.urls)),
+    path('', PhotoView.as_view(), name="home"),
+    path('feedback/', FeedbackView.as_view(), name="feedback"),
+    path('admin/', admin.site.urls)
 ]
