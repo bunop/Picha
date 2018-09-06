bee08c5e1bfdb66c1d2629b5990143d64fa8fb3d
Paolo Cozzi <bunop@libero.it>
2018-09-06 14:21:51 +0200

    Moving picha inside a docker-compose environment

--- picha/settings.py
+++ picha/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = 'Europe/Rome'
 
 USE_I18N = True
 
@@ -112,12 +112,12 @@ STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 # CELERY
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kolkata'
+CELERY_TIMEZONE = 'Europe/Rome'
 
 # Emails
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
